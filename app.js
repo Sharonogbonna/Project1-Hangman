6d442007140467666fe00739ae3c345aad1f5870
Sharon Ogbonna <114447309+Sharonogbonna@users.noreply.github.com>
2022-11-08 15:15:45 -0600

    basic html, css, and word options

---
+++ app.js
@@ -0,0 +1,35 @@
+//Topic options for the game
+let options = {
+    neuroanatomy: [
+        'Dendron',
+        'Neuron', 
+        'Synapse',
+        'nodes of Ranvier',
+        'Axon',
+        'Cerebral Cortex',
+        'Central Nervous System',
+        'Brain',
+        'Pia Mater',
+        'Amygdala',
+        'Soma',
+        'Blood Brain Barrier',
+        'Myelin Sheath'
+    ],
+    kitchenUtensils: [
+        'Spatula',
+        'Sauce Pan',
+        'Pot',
+        'Tongs',
+        'Ladle',
+        "Chef's Knife",
+        'Cutting Board', 
+        'Cheese Grater', 
+        'Peeler', 
+        'Tenderizer',
+        'Pizza Cutter',
+        'Mezzaluna',
+        'Bench Scraper',
+        'Sifter'
+    ]
+}
+
